bfea6c22ff6c49616b74d2b458326a39689c5b1f
Bojan Antonovic <bojan@antonovic.ch>
2021-12-21 18:35:19 +0100

    Initial commit

---
+++ src/main/java/helloworld/SpringBootMain.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMain.class, args);
+
+		// use the following URLs to test
+		// http://localhost:8080
+	}
+
+}
